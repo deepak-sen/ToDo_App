99ae2f0d18ea3abe57b03ad07e6cb71cf80bc175
deepak-sen <deepaksen454@gmail.com>
2020-06-13 16:55:29 +0545

    fragments pushed to fragment

---
+++ app/src/main/java/com/example/todomvvm/tasks/DatePickerFragment.java
@@ -0,0 +1,12 @@
+package com.example.todomvvm.tasks;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
